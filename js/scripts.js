16502557c8a470326480befe77cc3b2a1635c0be
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-25 08:55:00 -0700

    Add jquery to target the value of the radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-  
+  $("handedness-submit").submit(function(event){
+      var leftHand = $("input:radio[name=lefthand]:checked").val();
+      var rightHand = $("input:radio[name=righthand]:checked").val();
+
+
+      // if ($("input:radio[name=lefthand]:checked").val();)
+
+    event.preventDefault();
+  })
+
+
+
 
 })

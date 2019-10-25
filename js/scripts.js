5a6127f72d23dc757181df560148f72a1ab32f8c
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-25 09:28:45 -0700

    Add text field for finger count question. Fumble with toggling radio buttons and failing.

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,25 @@ $(document).ready(function(){
       var leftHand = $("input:radio[name=lefthand]:checked").val();
       var rightHand = $("input:radio[name=righthand]:checked").val();
 
+      function valueChanged() {
+    if ($("#lefthand").checked == true) {
+        var leftHand = $("input:radio[name=lefthand]:checked").val();
+        document.getElementById("is_Mercedes").value = 0;
+    } else {
+        document.getElementById("is_BMW").value = 0;
+        document.getElementById("is_Mercedes").value = 1;
+    }
+    console.log(document.getElementById("is_BMW").value);
+    console.log(document.getElementById("is_Mercedes").value)
+}
+
+
+
 
       // if ($("input:radio[name=lefthand]:checked").val();)
 
     event.preventDefault();
-  })
+  });
 
 
-
-
-})
+});

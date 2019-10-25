a23e4060f6a51a37e5308c279acc3a28eec629dc
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-25 09:51:12 -0700

    put containters around each form, tweek some css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,50 @@
+// $(document).ready(function(){
+//   $("handedness-submit").submit(function(event){
+//       var leftHand = $("input:radio[name=lefthand]:checked").val();
+//       var rightHand = $("input:radio[name=righthand]:checked").val();
+//
+//       function valueChanged() {
+//     if ($("#lefthand").checked == true) {
+//         var leftHand = $("input:radio[name=lefthand]:checked").val();
+//         document.getElementById("is_Mercedes").value = 0;
+//     } else {
+//         document.getElementById("is_BMW").value = 0;
+//         document.getElementById("is_Mercedes").value = 1;
+//     }
+//     console.log(document.getElementById("is_BMW").value);
+//     console.log(document.getElementById("is_Mercedes").value)
+// }
+//
+//
+//
+//
+//       // if ($("input:radio[name=lefthand]:checked").val();)
+//
+//     event.preventDefault();
+//   });
+//
+//
+// });
+
+
+
 $(document).ready(function(){
-  $("handedness-submit").submit(function(event){
-      var leftHand = $("input:radio[name=lefthand]:checked").val();
-      var rightHand = $("input:radio[name=righthand]:checked").val();
-
-      function valueChanged() {
-    if ($("#lefthand").checked == true) {
-        var leftHand = $("input:radio[name=lefthand]:checked").val();
-        document.getElementById("is_Mercedes").value = 0;
-    } else {
-        document.getElementById("is_BMW").value = 0;
-        document.getElementById("is_Mercedes").value = 1;
-    }
-    console.log(document.getElementById("is_BMW").value);
-    console.log(document.getElementById("is_Mercedes").value)
-}
-
-
-
-
-      // if ($("input:radio[name=lefthand]:checked").val();)
-
+  $("#fingerform").submit(function(event){
     event.preventDefault();
-  });
+    var fingers = parseInt($("#fingercount").val());
+    var eyes = $("select#gender").val();
 
 
+    if (age) {
+       var quote = 1000 - age;
+       if (gender === 'male' && age < 56) {
+         quote += 250;
+       }
+       $("#rate").empty().append(quote);
+       $("#quote").show();
+     } else {
+       alert('Please enter your age.');
+     }
 });
+
+  });

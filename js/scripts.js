59cc3a76155ac783c66f4f826ecf865a09bada03
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-25 15:06:16 -0700

    Add hand image, take steps in making else/if fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var hand = $("input:radio[name=hand]:checked").val();
     var fingers = parseInt($("#fingercount").val());
     var fingersFingers = parseInt($("#fingercount2").val());
-    var organ= $("input:radio[name=organ]:checked").val();
+    var organ = $("input:radio[name=organ]:checked").val();
     var feetFingers = parseInt($("#feet").val());
     // alert(hand);
     // alert(fingers);
@@ -12,27 +12,19 @@ $(document).ready(function(){
     // alert(organ);
     // alert(feetFingers);
 
-
-
-    // if (fingers >= fingersFingers) {
-    //   $("span#language").empty().append("C#");
-    //   $("#output-div").show();
-    // }
-
-    // if (hand === "lefthand") {
-    //
-    //   $("span#language").empty().append("Ruby");
-    //   $("#output-div").show();
-    //       }
-
-    if ((hand === "lefthand")) {
+    $("#output-div").show();
+    if (hand === "lefthand") {
       $("span#language").empty().append("Ruby");
       $("#output-div").show();
-    } else if (fingers >= 10) {
+    } else if (fingers >= 3 && fingersFingers >= 3) {
       $("span#language").empty().append("C#");
       $("#output-div").show();
+    } else if (organ === "liver") {
+      $("span#language").empty().append("Python");
+      $("#output-div").show();
     } else {
-      //  block of code to be executed if the condition1 is false and condition2 is false
+      $("span#language").empty().append("Java");
+      $("#output-div").show();
     }
 
 
